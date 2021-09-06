51dd0e7055b01e04a2186632cca0d62543e587c3
SohanChougule <sohan.chougule@cuelogic.com>
2021-09-06 20:27:22 +0530

    add task function in home page

--- js/validate.js
+++ js/validate.js
@@ -1,7 +1,17 @@
+
+if(JSON.parse(localStorage.getItem("users")) == null ){
+    localStorage.setItem("users" ,JSON.stringify([]));
+
+}
+if(JSON.parse(localStorage.getItem("task")) == null ){
+    localStorage.setItem("task" ,JSON.stringify([]));
+
+}
+
 function RegFormValidation() {
     console.log("in validate function")
     let uname = document.getElementById("uname");
-    
+
     let fname = document.getElementById("fname");
     let password = document.getElementById("password");
     let cpassword = document.getElementById("cpassword");
@@ -12,59 +22,128 @@ function RegFormValidation() {
     if (uname.value == "") {
         document.getElementById("unameerrmsg").innerText = "Please Enter User Name";
         fname.focus();
-    }else if (password.value == "") {
+    } else {
+        let arr = JSON.parse(localStorage.getItem("users"));
+        if (typeof arr !== null) {
+            for (let x of arr) {
+                if (x["uname"] == uname.value) {
+                    document.getElementById("unameerrmsg").innerText = "Username already taken please try another username";
+                    fname.focus();
+                }
+            }
+        }
+
+    }
+    if (password.value == "") {
         document.getElementById("passerrmsg").innerText = "Please Enter Password";
         password.focus();
-    }else if (cpassword.value == "" ) {
+    } else if (cpassword.value == "") {
         document.getElementById("passerrmsg").innerText = "Please Enter Confirm Password";
         cpassword.focus();
-    }else if (cpassword.value !== password.value ) {
+    } else if (cpassword.value !== password.value) {
         document.getElementById("passerrmsg").innerText = "Password and Confirm Password must match";
-    }else if (fname.value == "") {
+    } else if (fname.value == "") {
         document.getElementById("fnameerrmsg").innerText = "Please Enter First Name";
         fname.focus();
-    }else if (fname.value == "") {
+    } else if (fname.value == "") {
         document.getElementById("fnameerrmsg").innerText = "Please Enter First Name";
         fname.focus();
     }
     else if (lname.value == "") {
         lname.focus();
         document.getElementById("lnameerrmsg").innerText = "Please Enter Last Name";
-    }else if (gender.value == "") {
+    } else if (gender.value == "") {
         document.getElementById("gendererrmsg").innerText = "Please Select gender";
         gender.focus();
     }
     else if (address.value == "") {
         address.focus();
         document.getElementById("adderrmsg").innerText = "Please Enter Address";
-    } 
-    else {
+    } else {
+
         confirm("Are you sure ?")
-        localStorage.setItem("uname",uname.value);
-        localStorage.setItem("password",password.value);
-        
-        localStorage.setItem("fname",fname.value);
-        localStorage.setItem("lname",lname.value);
-        localStorage.setItem("address",address.value);
-        localStorage.setItem("gender",gender.value);
-        
+        let obj = {
+            uname: uname.value,
+            password: password.value,
+            fname: fname.value,
+            lname: lname.value,
+            address: address.value,
+            gender: gender.value
+        }
+        let users = JSON.parse(localStorage.getItem("users"))
+        users.push(obj);
+
+        localStorage.setItem("users", JSON.stringify(users));
+
         window.location.href = "login.html"
 
     }
 }
 
-function LoginFormValidation(){
-    let uname = document.getElementById("luname").value;
-    let password = document.getElementById("lpassword").value;
-    if(localStorage.getItem("uname")==uname){
-        if(localStorage.getItem("password")==password){
-            sessionStorage.setItem("luname",uname);
-            window.location.href = "index.html";
-        }else{
-            document.getElementById("loginerrmsg").innerHTML = "password does not match";
+function LoginFormValidation() {
+    let uname = document.getElementById("luname");
+    let password = document.getElementById("lpassword");
+    let arr = JSON.parse(localStorage.getItem("users"));
+
+    if (uname.value == "") {
+        document.getElementById("loginerrmsg").innerText = "Please Enter User Name";
+        fname.focus();
+    } else if (password.value == "") {
+        document.getElementById("loginerrmsg").innerText = "Please Enter Password";
+        password.focus();
+    } else {
+        let count = 0;
+        for (let x of arr) {
+            if (x["uname"] == uname.value) {
+                if (x["password"] == password.value) {
+                    sessionStorage.setItem("luser", JSON.stringify(x));
+                    window.location.href = "index.html";
+                    sessionStorage.setItem("userid", count);
+                    break;
+                } else {
+                    document.getElementById("loginerrmsg").innerHTML = "incorrect password";
+                }
+            } else {
+                document.getElementById("loginerrmsg").innerHTML = "Invalid Username or password";
+            }
+            count++;
         }
+    }
+}
+
+function displayTask() {
+
+
+}
+
+function addTask() {
+    let tname = document.getElementById("taskName");
+    let tdetails = document.getElementById("taskdetails");
+    let tdate = document.getElementById("taskdate");
+    
+    if(tname.value == ""){
+        document.getElementById("taskerrmsg").innerHTML = "Please Enter Task";
+    }else if(tdetails.value == ""){
+        document.getElementById("taskerrmsg").innerHTML = "Please Enter Task details";
+    }else if(tdate.value == ""){
+        document.getElementById("taskerrmsg").innerHTML = "Please Enter Date";
     }else{
-        document.getElementById("loginerrmsg").innerHTML = "username not found";
+        let userid = sessionStorage.getItem("userid");
+        obj = {"userid": userid,
+                "task":tname.value,
+                "details":tdetails.value,
+            "date":tdate.value}
+        let task = JSON.parse(localStorage.getItem("task"));
+        task.push(obj)
+        localStorage.setItem("task",JSON.stringify(task));
+        window.location.reload();
     }
     
+
 }
+
+function logout() {
+    sessionStorage.clear();
+    window.location.href = "login.html";
+}
+
