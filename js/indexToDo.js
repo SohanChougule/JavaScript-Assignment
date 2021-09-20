edb24c0f16f54ce26f3f1f47992eb87b1ef62566
SohanChougule <sohan.chougule@cuelogic.com>
2021-09-20 16:43:29 +0530

    mark todays task as done

--- js/indexToDo.js
+++ js/indexToDo.js
@@ -1,6 +1,7 @@
 function displayTask() {
     isSession();
     viewReminder();
+    viewToday();
     let arr = JSON.parse(localStorage.getItem("task"));
     let userid = sessionStorage.getItem("userid");
     let txt = "<th>Task name</th><th>Task details</th><th>Task date</th><th>Status</th><th>Public</th><th>Reminder</th><th>Reminder Date</th>";
@@ -145,6 +146,80 @@ function isReminder() {
         document.getElementById("taskReminderdate").disabled = false;
     }
 }
+
+function viewToday() {    
+    let txt = "<tr><th>Task name</th><th>Task details</th><th>Task date</th><th>Task Status</th><th>Public or Private</th><th>Reminder</th><th>Reminder Date</th><th>Mark as Done</th></tr>";
+    let today = new Date();
+    var dd = today. getDate();
+    var mm = today. getMonth()+1;
+    var yyyy = today. getFullYear();
+
+    if(mm<10){
+        mm="0"+mm
+    }
+
+    let date1 = yyyy+"-"+mm+"-"+dd;
+    let cnt = 0;
+    let arr = JSON.parse(sessionStorage.getItem("task"));
+    let userid = sessionStorage.getItem("userid");
+    for (let x of arr) {
+        if(x["userid"] == userid){
+            if(x["status"]=="pending"){
+                if((x["date"]) == (date1)) {
+                    cnt++
+                    txt += "<tr><td>" + x["task"] + "</td><td>" + x["details"] + "</td><td>" + x["date"] + "</td><td>" + x["status"] + "</td><td>" + x["isPublic"] + "</td><td>" + x["isReminder"] + "</td><td>" + x["reminderDate"] + "</td><td> <input type='checkbox' value=" + x["taskid"]+" name='doCheck' ></td> </tr>";
+                }
+            }
+                        
+            
+        }
+    }
+    if(cnt==0){
+        txt = "No Task For Today";
+    }
+
+    document.getElementById("TodayTable").innerHTML = txt;
+}
+
+
+function DoTask(){
+    let doItems = document.getElementsByName("doCheck");
+
+    console.log("length = ",doItems.length);
+    let tasks = JSON.parse(sessionStorage.getItem("task"));
+
+    for(let x of doItems){
+        for(let y of tasks){
+            if(x.value == y["taskid"]){
+                if(x.checked){
+                    y["status"] = "done";
+                }
+            }
+        }
+    }
+    
+    sessionStorage.setItem("task",JSON.stringify(tasks));
+
+    
+    userid = sessionStorage.getItem("userid");
+    ltask = JSON.parse(localStorage.getItem("task"));
+    for(let x of ltask){
+        if(x["userid"] == userid){
+            for(y of tasks){
+                if(y["taskid"] == x["taskid"]){
+                    x["status"] = y["status"];
+                }
+            }       
+        }
+    }
+    
+    localStorage.setItem("task",JSON.stringify(ltask));
+
+    window.location.reload();
+
+}
+
+
 
 function isSession() {
     userid = sessionStorage.getItem("userid");
